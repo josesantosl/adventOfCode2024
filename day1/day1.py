ec5616628bb477cddba6439a2586f916b42cc4d3
José Santos L. <josesantosl1607@gmail.com>
2024-12-09 11:02:15 +0100

    part 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+filename = "input.txt"
+
+# Generate 2 lists by the input
+l1 = []
+l2 = []
+with open(filename,'r') as file:
+    for line in file:
+        value1, value2 = map(int, line.split())
+        l1.append(value1)
+        l2.append(value2)
+
+#Sorting lists
+l1.sort()
+l2.sort()
+
+#print de sum of the minor
+print(sum([abs(l1[i]-l2[i])for i in range(len(l1))]))
