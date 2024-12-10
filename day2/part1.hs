91ecab21bbfc9a86f8e6aa84cc521e827f6526c2
José Santos L. <josesantosl1607@gmail.com>
2024-12-10 02:09:43 +0100

    day 2 : part 1
    adjust the README style.
    add the input.txt.
    change the language from python to haskell.

--- day2/part1.hs
+++ day2/part1.hs
@@ -1,3 +1,5 @@
+module Day2 where
+{-
 # day 2 : Red-Noses Reports
 ## part 1
 Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
@@ -7,8 +9,8 @@ While the Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no 
 They're quick to add that - since you're already here - they'd really appreciate your help analyzing some unusual data from the Red-Nosed reactor. You turn to check if The Historians are waiting for you, but they seem to have already divided into groups that are currently searching every corner of the facility. You offer to help with the unusual data.
 
 The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
- 
-``` 
+
+```
 7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
@@ -16,14 +18,39 @@ The unusual data (your puzzle input) consists of many reports, one report per li
 8 6 4 4 1
 1 3 6 7 9
 ```
+-}
 
+-- Read input from input.txt
+readInput :: IO [[Int]]
+readInput = do
+  file <- readFile "input.txt"
+  let numbers = map (map read . words) (lines file)
+  return numbers
+
+{-
 This example data contains six reports each containing five levels.
 
 The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
 
 - The levels are either all increasing or all decreasing.
 - Any two adjacent levels differ by at least one and at most three.
+-}
+-- confirm the safety of the report
+safetyWrapper :: [Int] -> Bool
+safetyWrapper (x1 : x2 : xs) | x1 == x2 = False
+                             | x1 > x2  = safety (x1:x2:xs) 0
+                             | x1 < x2  = safety (x1:x2:xs) 1
 
+safety :: [Int] -> Int -> Bool
+safety [_]              _ = True
+safety (x1 : x2 : xs) s | x1 == x2 = False
+                          | x1 > x2 && s == 0 && (abs(x1 - x2) <= 3) = safety (x2:xs) s
+                          | x1 > x2 && s == 1 || (abs(x1 - x2) > 3) = False
+                          | x1 < x2 && s == 0 || (abs(x1 - x2) > 3) = False
+                          | x1 < x2 && s == 1 && (abs(x1 - x2) <= 3) = safety (x2:xs) s
+
+
+{-
 In the example above, the reports can be found safe or unsafe by checking those rules:
 
 - `7 6 4 2 1`: **Safe** because the levels are all decreasing by 1 or 2.
@@ -36,5 +63,15 @@ In the example above, the reports can be found safe or unsafe by checking those 
 So, in this example, `2` reports are **safe**.
 
 **Analyze the unusual data from the engineers. How many reports are safe?**
+-}
 
+main :: IO ()
+main = do
+  input <- readInput
+  let counter = length (filter safetyWrapper input)
+  putStrLn (show counter)
+
+
+{-
 > Answer: 220
+-}
